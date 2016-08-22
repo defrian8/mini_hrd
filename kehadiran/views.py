d5e2be2e7df494e31366fd6495135804e67237f4
franschaniago <franschaniago5@gmail.com>
2016-08-22 15:47:30 +0700

    project 90%

--- kehadiran/views.py
+++ kehadiran/views.py
@@ -40,8 +40,22 @@ def pengajuan_izin(request):
 
     return render(request, 'tambah_izin.html', {'form':form})
 
+@login_required(login_url=settings.LOGIN_URL)
+def tampil_grafik(request, bulan, tahun):
+    temp_chart_data = []
+    daftar_hadir = Kehadiran.objects.filter(waktu__year=tahun, waktu__month=bulan, karyawan__id=request.session['karyawan_id'])
+
+    temp_chart_data.append({ "x":"hadir", "a":daftar_hadir.filter(jenis_kehadiran='hadir').count() })
+    temp_chart_data.append({ "x":"izin", "a":daftar_hadir.filter(jenis_kehadiran='izin').count() })
+    temp_chart_data.append({ "x":"alpa", "a":daftar_hadir.filter(jenis_kehadiran='alpa').count() })
+    temp_chart_data.append({ "x":"cuti", "a":daftar_hadir.filter(jenis_kehadiran='cuti').count() })
+
+    chart_data = json.dumps({"data":temp_chart_data})               
+
+    return render(request, 'new/tampil_grafik.html', {'chart_data':chart_data, 'bulan':bulan, 'tahun':tahun})
+
 
 @login_required(login_url=settings.LOGIN_URL)
 def daftar_izin(request):
-    daftar_izin = Izin.objects.filter(karyawan__id=request.session['karyawan_id'])
+    #daftar_izin = Izin.objects.filter(karyawan__id=request.session['karyawan_id'])
     return render(request, 'daftar_izin.html', {'daftar_izin':daftar_izin})
